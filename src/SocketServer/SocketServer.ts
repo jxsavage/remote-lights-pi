2a034aa1f84d00b40b69a2853f61b00ea56242b3
Jake Savage <website-repos@peek.com>
2020-04-08 21:30:24 -0600

    cleaned up socket server and added usage of reducer actions

--- src/SocketServer/SocketServer.ts
+++ src/SocketServer/SocketServer.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io';
 import { WebMicroInfo } from '../Shared/MicroTypes';
-import remoteLights, {RemoteLightsState, resetState, AddMicrosStateAction, addMicros} from '../Shared/reducers/remoteLights';
+import remoteLights, {RemoteLightsState, resetState, AddMicrosStateAction, addMicros, StateActions} from '../Shared/reducers/remoteLights';
 const initialState = {
   allMicroIds: [],
   byMicroId: {}
@@ -24,16 +24,10 @@ class PiServer {
     this.server
       .of('/server')
       .on('connection', (socket: SocketIO.Socket) => {
-        socket.on('addMicros', (microIdArr) => {
-          const addedMicros = this.filterNewMicros(microIdArr);
-          this.micros = this.micros.concat(addedMicros);
-          addedMicros.map((micro) => {
-            socket.join(micro.id);
-          })
-        });
+        socket.on('reInitAppState', () => socket.broadcast.emit('reInitAppState'));
         socket.on('initLightClient', (addMicrosAction: AddMicrosStateAction) => {
           socket.join('lightClients');
-          this.state = remoteLights(this.state, addMicrosAction);
+          this.handleStateAction(addMicrosAction);
           this.state.allMicroIds.forEach((microId) => {
             socket.join(microId);
           });
@@ -46,44 +40,18 @@ class PiServer {
           const resetStateAction = resetState({state});
           socket.emit('remoteLightsStateAction', resetStateAction);
         });
-        socket.on('getMicroBrightness', ({microId}) => {
-          socket.to(microId).emit(`getBrightness.${microId}`, socket.id);
+        socket.on('remoteLightsStateAction', (stateAction: StateActions) => {
+          this.handleStateAction(stateAction);
+          socket.broadcast.emit('remoteLightsStateAction', stateAction);
         });
-        socket.on('getMicroSegments', ({microId}) => {
-          socket.to(microId).emit(`getSegments.${microId}`, socket.id);
-        });
-        socket.on('getMicroEffect', ({microId}) => {
-          socket.to(microId).emit(`getEffect.${microId}`, socket.id);
-        });
-        socket.on('setMicroBrightness', ({microId, brightness}) => {
-          socket.to(microId)
-            .emit(`setBrightness.${microId}`, brightness);
-          socket.broadcast.to('webClients')
-            .emit(`setBrightness.${microId}`, brightness);
-        });
-        socket.on('setWebBrightness', ({microId, brightness, socketId}) => {
-          socket.to(socketId).emit(`setBrightness.${microId}`, brightness);
-        });
-        socket.on('setMicroEffect', ({microId, effect}) => {
-          socket.to(microId).emit(`setEffect.${microId}`, {microId, effect});
-          socket.broadcast.to('webClients').emit(`setEffect.${microId}`, effect);
-        });
-        socket.on('setWebEffect', ({microId, effect, socketId}) => {
-          socket.to(socketId).emit(`setEffect.${microId}`, effect);
-        });
-        socket.on('setWebSegments', ({microId, segments, socketId}) => {
-          socket.to(socketId).emit(`setSegments.${microId}`, segments);
-        });
-        const setWebInfo = (webInfoArr: WebMicroInfo[]) => {
-
-        }
-        socket.on('setWebInfo', setWebInfo);
-        this.onGetMicrosListener(socket);
         socket.on('disconnect', () => {
           console.log('socket disconnected', socket.id);
           if (this.webClients.has(socket.id)) this.webClients.delete(socket.id);
         });
       });
+  }
+  handleStateAction = (stateAction: StateActions) => {
+    this.state = remoteLights(this.state, stateAction);
   }
   filterNewMicros = (newMicroInfoArr: WebMicroInfo[]) => {
     const {micros} = this;
@@ -93,23 +61,6 @@ class PiServer {
       });
       return hasMicro == -1;
     });
-  }
-  addMicros = (microIdArr: string[]) => {
-  }
-  onGetMicrosListener = (socket: SocketIO.Socket) => {
-    const micros = this.micros;
-    this.joinMicroChannels(socket, micros);
-    socket.on('getMicros', () => {
-      socket.emit('setMicros', micros);
-    });
-  }
-  joinMicroChannels = (socket: SocketIO.Socket, microArr: WebMicroInfo[]) => {
-    microArr.forEach((micro) => {
-      socket.join(micro.id);
-    })
-  }
-  addClient(clientConfig: object) {
-
   }
 }
 export default PiServer;
